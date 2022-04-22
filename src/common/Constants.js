2430036228973274ad355f36be38d5fd860b5620
Manvendra P Singh <manvendra.singh@gmail.com>
2022-04-22 19:45:25 -0400

    adding gh-deploy & typo fixed

--- src/common/Constants.js
+++ src/common/Constants.js
@@ -1,5 +1,5 @@
 export const URL_DEV = 'http://localhost:3001/items';
 export const URL_PROD = 'https://www.googleapis.com/blogger/v3/blogs/4673844896386998977/posts';
-export const queryParam = 'orderBy=updated&status=live&maxResults=12&fetchImages=trueç';
+export const queryParam = 'orderBy=updated&status=live&maxResults=12&fetchImages=true';
 export const key ='AIzaSyA0I1s-HlvF_15Z8yipg2UVOIgzMWr73vI'
 
