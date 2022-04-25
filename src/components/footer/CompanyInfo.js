5e15a97a3c805a46d7bd72d6074e04d4a4e11b0e
Manvendra P Singh <manvendra.singh@gmail.com>
2022-04-25 01:48:47 -0400

    colors in name & quote update

--- src/components/footer/CompanyInfo.js
+++ src/components/footer/CompanyInfo.js
@@ -2,7 +2,7 @@ const CompanyInfo = () => {
 	return (
 		<div className="company-info">
 			<p> "I do what I do, because I do what I do.
-				I know, the much I do isn't worth doing.
+				Some say that, the much I do isn't worth doing.
 				But I will still do what I do, because, I do what I do."
 				<p className='quote'>
 				- Manvendra P Singh
--- src/components/homepage/Header.js
+++ src/components/homepage/Header.js
@@ -3,7 +3,20 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-left">
-        <div className='company-name'>kaleidoscope</div>
+        <div className='company-name'>
+          <font color="#FF2626">K</font>
+          <font color="#252A34">a</font>
+          <font color="#753422">l</font>
+          <font color="#FFD523">e</font>
+          <font color="#FF9966">i</font>
+          <font color="#0F52BA">d</font>
+          <font color="#66CC66">o</font>
+          <font color="#FF9966">s</font>
+          <font color="#FFD523">c</font>
+          <font color="#00C1D4">o</font>
+          <font color="#252A34">p</font>
+          <font color="#FF2626">e</font>
+        </div>
         <div className='quote'>
           - A little something about many different things.
         </div>
