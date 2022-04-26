d3980edef7ac601aeec81ab0db0b263c7a56e199
Manvendra P Singh <manvendra.singh@gmail.com>
2022-04-26 15:27:01 -0400

    Added postList sort by rank

--- src/common/Utils.js
+++ src/common/Utils.js
@@ -1,3 +1,5 @@
+import  * as Constants from './Constants'
+
 export const extractOverviewFromContent = (content) => {
     let template = document.createElement('template');
     template.innerHTML = content;
@@ -8,4 +10,15 @@ export const appendContent = (parent,content) =>{
     var contentDiv = document.createElement("div");
     contentDiv.innerHTML = content;
     parent.appendChild(contentDiv);
+}
+
+export  const sortPostByRank = (posts) =>
+posts.sort((post1, post2) => getPostRank(post1.labels) >= getPostRank(post2.labels) ? 1 : -1);
+
+export const getPostRank = (labels) => {
+let rankLabel = labels?.find(label => label.startsWith(Constants.POST_LABEL_RANK));
+
+return (rankLabel)
+  ? parseInt(rankLabel.substring(rankLabel.indexOf(Constants.POST_LABEL_RANK) + Constants.POST_LABEL_RANK.length))
+  : 1000; //A random high number to have unranked post at the end
 }
--- src/components/blog-post/PostsList.js
+++ src/components/blog-post/PostsList.js
@@ -3,15 +3,16 @@ import './PostsList.css'
 import axios from "axios";
 import { useEffect, useState } from "react";
 import * as Constants from '../../common/Constants'
+import * as Utils from '../../common/Utils'
 
 const PostsList = () => {
   const [postDataList, setPostDataList] = useState([]);
-  const url = Constants.URL_PROD + '?' + Constants.queryParam + '&key=' + Constants.key;
+  const url = Constants.URL_PROD + '?' + Constants.POST_LIST_QRY_PARAM + '&key=' + Constants.key;
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const response = await axios.get(url);
-        setPostDataList(response.data.items);
+        setPostDataList(Utils.sortPostByRank(response.data.items));
       } catch (err) {
         console.error("error in calling backend");
         if (err.response) {
