a7d5bdaa91b755e372e17bf989ad926af26662f0
Manvendra P Singh <manvendra.singh@gmail.com>
2022-04-27 18:42:05 -0400

    add blogpost label and responsiveness

--- src/components/blog-post/BlogPosts.js
+++ src/components/blog-post/BlogPosts.js
@@ -6,7 +6,10 @@ const BlogPosts = () => {
         <div className='blog-post'>
 
             <div className='blog-post-description'>
-                &nbsp;
+                <span>
+                    These are my posts from Medium or Google Blogger 
+                    <div className="quote"> -- Search is available only for google blogs.</div>     
+                </span>
             </div>
             <div className='search-posts' >
                 <input defaultValue='Search all posts ...' type='text' />
