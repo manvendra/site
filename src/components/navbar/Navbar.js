e3aecaf62d6296afa22616d8f52ee3cb2bf60ff3
Manvendra P Singh <manvendra.singh@gmail.com>
2022-05-13 12:33:07 -0400

    Responsive Navbar without any library

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,62 +1,45 @@
 import logo from '../../images/logo/logo.png';
+import hamburger from '../../images/hamburger.png';
 import './Navbar.css'
-import { useState } from "react";
-import {
-  MDBNavbar,
-  MDBContainer,
-  MDBIcon,
-  MDBNavbarNav,
-  MDBNavbarItem,
-  MDBNavbarLink,
-  MDBNavbarToggler,
-  MDBCollapse,
-  MDBNavbarBrand
-} from "mdb-react-ui-kit";
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
 
 const Navbar = () => {
-  const [showNavRight, setShowNavRight] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', changeWidth)
+
+    return () => { window.removeEventListener('resize', changeWidth) }
+  }, [])
+
   return (
-    <MDBNavbar className="hover-shadow sticky" expand="lg" light >
-      <MDBContainer fluid>
-        <MDBNavbarBrand className="ms-5 bs-5" tag={Link}  to="/home">
+    <nav class="navbar">
+
+      <div class="brand-title">
+        <Link to="/home">
           <img src={logo} height="40" alt="" loading="lazy" />
-        </MDBNavbarBrand>
+        </Link>
+      </div>
 
+      <img src={hamburger} alt="" loading="lazy"
+        className='toggle-button'
+        onClick={() => setToggleMenu(!toggleMenu)} />
 
-        <MDBNavbarToggler
-          type="button"
-          data-target="#navbarRightAlignExample"
-          aria-controls="navbarRightAlignExample"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          onClick={() => setShowNavRight(!showNavRight)}>
-          <MDBIcon icon="bars" fas />
-        </MDBNavbarToggler>
+      {(toggleMenu || screenWidth > 768) && (
+        <ul class="navbar-links">
+          <li class="navbar-link"><Link to="/home" >Home</Link></li>
+          <li class="navbar-link"><Link to="/posts">BlogPosts</Link></li>
+          <li class="navbar-link"><Link to="/photos">Photography</Link></li>
+          <li class="navbar-link"><Link to="/about" >About Me</Link></li>
+        </ul>
 
-        <MDBCollapse navbar show={showNavRight}>
-          <MDBNavbarNav right fullWidth={false} className="mb-2 mb-lg-0 ">
-
-            <MDBNavbarItem role="button" className="me-3 ">
-              <MDBNavbarLink tag={Link}  to="/home">Home</MDBNavbarLink>
-            </MDBNavbarItem>
-            
-            <MDBNavbarItem role="button" className="me-3">
-              <MDBNavbarLink tag={Link} to="/posts">BlogPosts</MDBNavbarLink>
-            </MDBNavbarItem>
-            
-            <MDBNavbarItem role="button" className="me-3">
-              <MDBNavbarLink tag={Link} to="/photos">Photography</MDBNavbarLink>
-            </MDBNavbarItem>
-            
-            <MDBNavbarItem role="button" className="me-3">
-              <MDBNavbarLink tag={Link} to="/about" >About Me</MDBNavbarLink>
-            </MDBNavbarItem>
-          
-          </MDBNavbarNav>
-        </MDBCollapse>
-      </MDBContainer>
-    </MDBNavbar>
+      )}
+    </nav>
   );
 };
 export default Navbar;
