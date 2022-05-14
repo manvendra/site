5a9ddb338353919a9dfad9548cc92d28187203f4
Manvendra P Singh <manvendra.singh@gmail.com>
2022-05-13 21:33:31 -0400

    Footer - Contact Me UI only

--- src/components/footer/ContactInfo.js
+++ src/components/footer/ContactInfo.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
+
 const ContactInfo = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`name = ${name} , email= ${email} , message = ${message}`)
+  }
   return (
-    <div className="contact-info">
-      <p><i className="fas fa-home"></i> New Jersey, USA</p>
-      <p><i className="fas fa-phone"></i> + 01 201 965 9837</p>
-      <p><i className="fas fa-envelope"></i> manvendra.singh@gmail.com</p>
+    <div className="contact-me">
+      <form onSubmit={handleSubmit} autocomplete="off">
+        <input type="text"
+          name='name'
+          value={name}
+          placeholder='Your name ...'
+          onChange={e => setName(e.target.value)} />
+        <input type="email"
+          name='email'
+          value={email}
+          placeholder='Your email ...'
+          required
+          onChange={e => setEmail(e.target.value)} />
+        <textarea name='message'
+          value={message}
+          placeholder='Your comments or message. Be Nice !'
+          required rows={3} maxLength={200}
+          onChange={e => setMessage(e.target.value)} />
+        <div className="content-right">
+          <input className="red-button round-button" type="submit" value="Send Feedback" />
+        </div>
+      </form>
     </div>
   );
 };
