dc98ec2f5274880e6b18d21a22e0d25da2a71378
Manvendra P Singh <manvendra.singh@gmail.com>
2022-05-19 00:05:21 -0400

    Added Donation mechanism

---
+++ src/components/footer/Donate.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Donate() {
+    return (
+        <div className='donate'>
+            <div className='paypal'>
+                <form action="https://www.paypal.com/donate" method="post" target="_blank">
+                    <input type="hidden" name="business" value="3N2V583ZR5LEN" />
+                    <input type="hidden" name="no_recurring" value="1" />
+                    <input type="hidden" name="currency_code" value="USD" />
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif"
+                        border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                </form>
+            </div>
+            <div className='buy-me-coffee'>
+                <a href="https://www.buymeacoffee.com/manvendrasu"
+                    target="_blank">
+                    <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+                        alt="Buy Me A Coffee"  />
+              </a>
+            </div>
+        </div>
+    )
+}
+
+export default Donate
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,12 +2,14 @@ import './Footer.css';
 import CompanyInfo from "./CompanyInfo";
 import ContactMe from "./ContactMe";
 import FooterIcon from "./FooterIcons"
+import Donate from './Donate';
 const Footer = () => {
   return (
     <div className='appFooter'>
       <CompanyInfo />
       <ContactMe />
       <FooterIcon/> 
+      <Donate/>
       <div className="bottom">
         <span>© 2022 Copyright: Manvendra P Singh</span>
       </div>
