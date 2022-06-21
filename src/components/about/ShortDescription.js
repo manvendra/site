ac9b33fd939b83fd3ef54e1dfa1f82141bc78ad4
Manvendra P Singh <manvendra.singh@gmail.com>
2022-06-21 10:37:17 -0400

    about-me updated

--- src/components/about/ShortDescription.js
+++ src/components/about/ShortDescription.js
@@ -18,15 +18,15 @@ function ShortDescription() {
             <ul>
                 <li>
                     I've worked on various enterprise-scale solutions for different clients,
-                    spanning across Finance, Automotive, Healthcare, Products & Travel domains in 15 years <i>(and counting)</i>.
+                    spanning across Finance, Automotive, Healthcare, Products & Travel domains. Fifteen years <i>(and counting)</i>.
                 </li>
                 <li>
-                    Usually I'm the culprit for messing up the architecture & design of backend systems.
+                    Usually I'm the culprit behind the architectural & design flaw of backend systems.
                     And most of the time, if not always, the contributor of the highest number of code defects.
                 </li>
                 <li>
                     For the last few years,
-                    I've been damaging various cloud-based Greenfield and Brownfield projects
+                    I've been harming various cloud-based Greenfield and Brownfield projects
                     using AWS, PCF, K8S, Docker, SpringBoot, Kafka, ELK Stack, Cassandra,
                     and a few more fancy names.
                 </li>
@@ -38,16 +38,16 @@ function ShortDescription() {
                 </li>
             </ul>
             <br />
-            <h5> Some victims of my works:</h5>
+            <h5> Some prominent victims of my works:</h5>
             <div className='clients'>
-                <img className='img-l' src={tc} alt='client' />
-                <img className='img-s' src={bmw} alt='client' />
-                <img className='img-m' src={cmcst} alt='client' />
-                <img className='img-s' src={one} alt='client' />
-                <img className='img-l' src={xpt} alt='client' />
-                <img className='img-s' src={itrtk} alt='client' />
-                <img className='img-m' src={ub} alt='client' />
-                <img className='img-m' src={zcs} alt='client' />
+                <img className='img-l' src={tc} alt='Thomas Cook' />
+                <img className='img-s' src={bmw} alt='BMW' />
+                <img className='img-m' src={cmcst} alt='Comcast' />
+                <img className='img-s' src={one} alt='NSDL' />
+                <img className='img-l' src={xpt} alt='Express Scripts' />
+                <img className='img-s' src={itrtk} alt='Intertek' />
+                <img className='img-m' src={ub} alt='U.S. Bank' />
+                <img className='img-m' src={zcs} alt='Zycus' />
             </div>
         </p>
     )
