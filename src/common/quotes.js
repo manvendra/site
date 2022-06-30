9bac746130dd75f46c0a6285cf5db5089d875ac3
Manvendra P Singh <manvendra.singh@gmail.com>
2022-06-30 13:53:10 -0400

    saving quoted for future

---
+++ src/common/quotes.js
@@ -0,0 +1,10 @@
+export const Quotes = [
+    {
+        author: "Zig Zagler",
+        quote: "You don't have to be great to start, but you have to start to be great."
+    },
+    {
+        author: "Pluto",
+        quote: "I am the wisest man alive, for I know one thing, and that is that I know nothing."
+    }
+]
