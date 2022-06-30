8250b39e25d36a80b31510a56aa425ebda4a6b82
Manvendra P Singh <manvendra.singh@gmail.com>
2022-06-30 13:56:00 -0400

    Homepage quote author removal

--- src/components/homepage/HomeContent.js
+++ src/components/homepage/HomeContent.js
@@ -3,8 +3,8 @@ const HomeContent = () => {
     return (
         <div className="home-content">
             <blockquote className="multi-color-text"> 
-            &ldquo; I am the wisest man alive, for I know one thing, and that is that I know nothing &rdquo;
-                <footer className='quote' style={{ textAlign: 'right'}}>&mdash; Pluto</footer>
+            I am the wisest man alive, for I know one thing, and that is that I know nothing. 
+                {/* <footer className='quote' style={{ textAlign: 'right'}}>&mdash; Pluto</footer> */}
             </blockquote>
         </div>
     );
