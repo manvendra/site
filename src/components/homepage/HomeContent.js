cef51676eff4f04b5c50bfd5a1fcd5e5187c3c86
Manvendra P Singh <manvendra.singh@gmail.com>
2022-07-03 20:36:26 -0400

    home page styling changes

--- src/components/homepage/HomeContent.js
+++ src/components/homepage/HomeContent.js
@@ -2,24 +2,27 @@ import { Link } from "react-router-dom";
 import { SITE_MODULES as modules } from '../../common/Constants'
 
 const HomeContent = () => {
-	 
+
 	return (
 		<div className="content">
-	
+
 			{/* <div className="small-text container-description {">
 				This website is a  collection of my random interests. Feel free to navigate.
 				Here are some quick links.
 			</div> */}
-			<div className="cards-container">
+			<div className="zigzag-container">
 				{modules.map((module, index) => (
 					(module.homeEnabled) && (
-					<Link to={module.link} key={'home_content_section_'+index}>
-						<div className="card">
-						    <img src= {module.img} alt=".."></img>
-							<header> {module.title}</header>
-							<section> {module.shortDesc}</section>
+						<div className="horizontal-card">
+							<img src={module.img} alt={module.shortDesc}></img>
+							<div>
+								<header> {module.title}</header>
+								<div> {module.shortDesc}</div>
+								<Link to={module.link} key={'home_content_section_' + index}>
+									<button className="round-button blue-button">Show more</button>
+								</Link>
+							</div>
 						</div>
-					</Link>
 					)
 				))}
 			</div>
