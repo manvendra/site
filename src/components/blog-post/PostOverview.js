610865581ccfc4970ed797a1250c61eb64de6824
Manvendra P Singh <manvendra.singh@gmail.com>
2023-02-21 02:03:42 -0500

    moved back to light background

--- src/components/blog-post/PostOverview.js
+++ src/components/blog-post/PostOverview.js
@@ -20,7 +20,7 @@ const PostOverview = ({ postData }) => {
       <footer>
         <div>
         <Link to={postData.id} >
-          <button className="read-more round-button secondary-color-button">Read More</button>
+          <button className="read-more round-button tertiary-color-button ">Read More</button>
         </Link>
         </div>
       </footer>
--- src/components/photography/photos.js
+++ src/components/photography/photos.js
@@ -1,23 +1,63 @@
 export const photos = [
   {
-    src: 'https://source.unsplash.com/9z6ldWjo7eY/w=1920',
+    src: "https://source.unsplash.com/iAL4wp9TMxQ/",
+    width: 5,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/DqBcC718cEA/",
+    width: 3,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/9z6ldWjo7eY/',
+    width: 5,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/NFDwfbiFeKc/",
+    width: 4,
+    height: 3
+  },
+
+  {
+    src: "https://source.unsplash.com/o_M6J5BYloc/",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/1c7d_pMlVVw/w=1920",
-    width: 1,
-    height: 1
-  },
-
-  {
     src: "https://source.unsplash.com/abwJ_2g9t6k/600x799",
-    width: 3,
-    height: 4
+    width: 2.5,
+    height: 3
   },
   {
-    src: 'https://source.unsplash.com/6-afi_aUuDU/w=1920',
+    src: 'https://source.unsplash.com/6-afi_aUuDU/',
     width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/XfnSXmK2eS0/",
+    width: 6,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/W6sJqcKlHYk/",
+    width: 5,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/1c7d_pMlVVw/",
+    width: 3,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/pVVoNe2J-w8/800x599",
+    width: 5,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/d84v-ApM7os/",
+    width: 3,
     height: 3
   },
   {
@@ -26,8 +66,8 @@ export const photos = [
     height: 3
   },
   {
-    src: "https://source.unsplash.com/eFRLnTHTvPQ/600x799",
-    width: 4,
+    src: "https://source.unsplash.com/kfM1ER4LKuo/",
+    width: 5,
     height: 3
   },
   // {
@@ -40,10 +80,6 @@ export const photos = [
   //   width: 4,
   //   height: 3
   // },
-  {
-    src: "https://source.unsplash.com/pVVoNe2J-w8/800x599",
-    width: 5,
-    height: 3
-  },
+
 
 ];
